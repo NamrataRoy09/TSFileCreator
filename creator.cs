531da9dcb89f9e88424e06e9e27418a9525ab069
Namrata Roy <namrata.roy@planview.com>
2023-06-28 15:42:35 +0530

    File functionality added in creator

--- creator.cs
+++ creator.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace TSFileCreator
 {
-    internal class creator
+    public class creator
     {
         internal int someValue = 55;
         //comment added
         public string log = "Let's have fun!!";
+
+        public void complexcreator()
+        {
+            System.IO.Directory.CreateDirectory("C:\\FileLogger");
+            Byte[] title = new UTF8Encoding(true).GetBytes("New Text File");
+           
+        }
     }
 }
---
+++ deMigrator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSFileCreator
+{
+    internal class deMigrator
+    {
+        public deMigrator(int param1)
+        {
+                    
+        }
+    }
+}
