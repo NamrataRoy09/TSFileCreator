0781da5f7ddd2b157c78c5d672f52e064959e069
Namrata Roy <namrata.roy@planview.com>
2023-06-28 12:27:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+string fileName = @"C:\FileLogger\logger.txt";
+System.IO.Directory.CreateDirectory("C:\\FileLogger");
+
+using (FileStream fs = File.Create(fileName))
+{
+    // Add some text to file    
+    Byte[] title = new UTF8Encoding(true).GetBytes("New Text File");
+    fs.Write(title, 0, title.Length);
+    byte[] author = new UTF8Encoding(true).GetBytes("Mahesh Chand");
+    fs.Write(author, 0, author.Length);
+}
+
